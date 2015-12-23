352d2ff5cf5aa2ff98f4172f4d956e5af613c10e
gitbnw <byron.weiss@gmail.com>
2015-12-23 20:45:14 +0000

    Story 5: seeded data

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'rails', '4.2.4'
    gem 'rspec-rails', '~> 3.0'
    gem 'shoulda'
    gem 'factory_girl_rails', '~> 4.0'
+   gem 'faker'
  end
 
  # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
- include RandomData
+ include Faker
  
  # Create Wikis
  50.times do
+  
+  password = Faker::Internet.password
+  user = User.new(
+  :email => Faker::Internet.email,
+  :password => password,
+  :password_confirmation => password
+  )
+  user.skip_confirmation!
+  user.save!
+
+paragraphs = Faker::Lorem.paragraphs
+title = Faker::Lorem.word
 
    Wiki.create!(
 
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph,
-     private: false
+     title:  title.titlecase,
+     body:   paragraphs.join("\r\n\r\n"),
+     private: false,
+     user: user
      
    )
  end
+ 
+  user = User.new(
+  :email => 'byron.weiss@gmail.com',
+  :password => 'helloworld',
+  :password_confirmation => 'helloworld'
+  )
+  user.skip_confirmation!
+  user.save!
+ 
  wikis = Wiki.all
 
  
  puts "Seed finished"
  puts "#{Wiki.count} wikis created"
+ puts "#{User.count} users created"
